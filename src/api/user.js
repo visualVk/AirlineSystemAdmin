40486fa072128cf8527a019e3a7aee8556cf0875
Tom <2048137831@qq.com>
2021-04-13 14:51:05 +0800

    新建工程

---
+++ src/api/user.js
@@ -0,0 +1,32 @@
+import axios from '@/libs/api.request'
+
+export const login = ({ username, password }) => {
+  const data = {
+    username,
+    password,
+    isAdmin: true,
+    rememberMe: true
+  }
+  return axios.request({
+    url: '/api/user/auth/login',
+    data,
+    method: 'post'
+  })
+}
+//deprecated
+export const getUserInfo = (token) => {
+  return axios.request({
+    url: '/api/user/auth/getUserInfo',
+    // params: {
+    //   token
+    // },
+    method: 'post'
+  })
+}
+
+export const logout = (token) => {
+  return axios.request({
+    url: '/api/user/auth/logout',
+    method: 'post'
+  })
+}
---
+++ src/store/module/user.js
@@ -0,0 +1,94 @@
+import { login, logout, getUserInfo } from '@/api/user'
+import { setToken, getToken } from '@/libs/util'
+
+export default {
+  state: {
+    username: '',
+    userId: '',
+    avatorImgPath: '',
+    companyId: '',
+    token: getToken(),
+    access: '',
+    hasGetInfo: false
+  },
+  mutations: {
+    setAvator(state, avatorPath) {
+      state.avatorImgPath = avatorPath
+    },
+    setUserId(state, id) {
+      state.userId = id
+    },
+    setUsername(state, name) {
+      state.username = name
+    },
+    setAccess(state, access) {
+      state.access = access
+    },
+    setToken(state, token) {
+      state.token = token
+      setToken(token)
+    },
+    setCompanyId(state, companyId) {
+      state.companyId = companyId
+    },
+    setHasGetInfo(state, status) {
+      state.hasGetInfo = status
+    }
+  },
+  actions: {
+    // 登录
+    handleLogin({ commit }, { username, password }) {
+      username = username.trim()
+      return new Promise((resolve, reject) => {
+        login({
+          username,
+          password
+        }).then(res => {
+          const { data } = res.data
+          console.log('[Store]', '{login result}', data)
+          commit('setToken', data.token)
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+      })
+    },
+    // 退出登录
+    handleLogOut({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        logout(state.token).then(() => {
+          commit('setToken', '')
+          commit('setAccess', [])
+          resolve()
+        }).catch(err => {
+          reject(err)
+        })
+        // 如果你的退出登录无需请求接口，则可以直接使用下面三行代码而无需使用logout调用接口
+        // commit('setToken', '')
+        // commit('setAccess', [])
+        // resolve()
+      })
+    },
+    // 获取用户相关信息
+    getUserInfo({ state, commit }) {
+      return new Promise((resolve, reject) => {
+        try {
+          getUserInfo(state.token).then(res => {
+            const { data } = res.data
+            commit('setAvator', data.avator)
+            commit('setUsername', data.username)
+            commit('setUserId', data.uid)
+            commit('setAccess', data.access)
+            commit('setHasGetInfo', true)
+            console.log('[Store User Info]', state)
+            resolve(data)
+          }).catch(err => {
+            reject(err)
+          })
+        } catch (error) {
+          reject(error)
+        }
+      })
+    }
+  }
+}
